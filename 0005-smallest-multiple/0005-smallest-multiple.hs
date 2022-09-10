d5d9f50483f953c41add3a0320ea9b1600ea0071
Jeff Alkire <jeff@alkire.net>
2022-09-10 15:48:07 -0700

    solved problem 5

--- 0005-smallest-multiple/0005-smallest-multiple.hs
+++ 0005-smallest-multiple/0005-smallest-multiple.hs
@@ -1,9 +1,58 @@
 
+import qualified Data.List                      as Lst
 
 main :: IO ()
 main = do
     putStrLn ""
     putStrLn ""
     putStrLn "Working on euler problem #0005."
+    putStrLn ""
+
+    print $ findSmallestDivisibleByAll [1..20]
+
+-- find the smallest int divisible by all values if fs.
+findSmallestDivisibleByAll :: [Int] -> Int
+findSmallestDivisibleByAll fs =
+    let mxd = maximum fs  -- highest divisor in list
+    in  findSmallestDivisibleByAll' fs mxd mxd
+
+findSmallestDivisibleByAll' :: [Int] -> Int -> Int -> Int
+findSmallestDivisibleByAll' fs n step =
+    if isDivisibleByAll fs n then
+        n
+    else findSmallestDivisibleByAll' fs (n+step) step
 
 
+
+-- is x divisible by all numbers in ns?
+isDivisibleByAll :: [Int] -> Int -> Bool
+isDivisibleByAll ns x =
+    let fs = factor x
+    in  includesAll fs ns
+
+-- are all members of xs in ys?
+includesAll :: Eq a => [a] -> [a] -> Bool
+includesAll xs ys = and $ map (`elem` xs) ys
+
+factor :: Int -> [Int]
+factor i = reverse $ findFactorsOf i 1 []
+
+-- Int   - number to factor
+-- Int   - highest number considered
+-- [Int] - list of factors found
+findFactorsOf :: Int -> Int -> [Int] -> [Int]
+findFactorsOf n c found =
+    let done = c*c > n
+    in  if done then
+            found
+        else if isFactor n c then
+            let d = n `div` c
+            in  reverse $ Lst.sort $ findFactorsOf n (c+1) (c:d:found)
+        else
+            findFactorsOf n (c+1) found
+
+isFactor :: Int -> Int -> Bool
+isFactor n f = (n `mod` f == 0)
+
+isPrime :: Int -> Bool
+isPrime p = null (factor p)
