0a2db68a7b48d05c7988451729bfcc511ecb1c00
Jeff Alkire <jeff@alkire.net>
2022-09-13 15:38:42 -0700

    Solve 24

---
+++ 0024-lexicographic-permutations/0024-lexicographic-permutations.hs
@@ -0,0 +1,34 @@
+
+import qualified Data.Char                      as Ch
+import qualified Data.Text                      as Txt
+import qualified Data.List                      as Lst
+import qualified Data.List.Split                as Spl
+
+import qualified CmdLine                        as Cmd
+import qualified Factor                         as Ftor
+import qualified ListFns                        as Lstf
+
+
+main :: IO ()
+main = do
+    putStrLn ""
+    putStrLn ""
+    putStrLn "Working on euler problem #0024."
+    putStrLn ""
+
+    digCnt <- Cmd.numFromCommandLineArg 1
+    which  <- Cmd.numFromCommandLineArg 2
+
+
+    let digits = [0..digCnt]
+        perms = Lst.sort $ Lst.permutations digits
+        ans = perms !! (which-1)
+
+    putStr "Entry # "
+    putStr $ show digCnt
+    putStr " in the ordered list of permutations of "
+    putStr $ show digits
+    putStr " is "
+    putStr $ show ans
+    putStrLn "."
+
