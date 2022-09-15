ff8de034a75ec82e06f1e33c511dedf2560259d8
Jeff Alkire <jeff@alkire.net>
2022-09-14 23:12:00 -0700

    solve 35

---
+++ 0035-circular-primes/0035-circular-primes.hs
@@ -0,0 +1,20 @@
+
+import qualified Data.Char                      as Ch
+import qualified Data.Text                      as Txt
+import qualified Data.List                      as Lst
+import qualified Data.List.Split                as Spl
+
+import qualified CmdLine                        as Cmd
+import qualified Factor                         as Ftor
+import qualified ListFns                        as Lstf
+import qualified MathFns                        as Mthf
+
+
+main :: IO ()
+main = do
+    putStrLn ""
+    putStrLn ""
+    putStrLn "Working on euler problem #0035."
+    putStrLn ""
+
+
